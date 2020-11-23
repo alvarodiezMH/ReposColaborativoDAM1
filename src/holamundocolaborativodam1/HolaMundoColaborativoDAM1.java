ce8f401a8119354c56205a2f3d67a9dbfd00e0a3
adiezc <adiezc@IF03-00.iesmhp.local>
2020-11-23 16:19:14 +0100

    He añadido un println

--- src/holamundocolaborativodam1/HolaMundoColaborativoDAM1.java
+++ src/holamundocolaborativodam1/HolaMundoColaborativoDAM1.java
@@ -17,7 +17,7 @@ public class HolaMundoColaborativoDAM1 {
     public static void main(String[] args) {
         // TODO code application logic here
 // comentario de prueba
-        
+        System.out.println("Hola Mundo - Alvaro");
     }
     
 }
